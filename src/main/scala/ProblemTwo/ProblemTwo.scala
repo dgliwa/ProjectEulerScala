53d18961b26a788616a3c52cc7866d103a51d23b
Tyler Bobella <tylerbobella@Tylers-MacBook-Pro.local>
2014-09-04 23:47:41 -0600

    Problem two is finished

---
+++ src/main/scala/ProblemTwo/ProblemTwo.scala
@@ -0,0 +1,25 @@
+package ProblemTwo
+
+/**
+ * Created by tylerbobella on 9/4/14.
+ */
+class ProblemTwo {
+
+  def fibSeq(first: Int, second: Int): Int = first match{
+    case 0 => first
+
+    case _ =>
+      (if (second % 2 == 0 ){
+         second
+       } else {
+         0
+       }) + (if (first+second > 4000000){
+        0
+      } else {
+        fibSeq(second, first + second)
+      })
+  }
+
+
+
+}
---
+++ src/main/scala/ProblemTwo/ProblemTwoObject.scala
@@ -0,0 +1,15 @@
+package ProblemTwo
+
+/**
+ * Created by tylerbobella on 9/4/14.
+ */
+object ProblemTwoObject {
+
+  def main(args: Array[String]): Unit ={
+    val functionalProblemTwo = new ProblemTwo
+
+
+    functionalProblemTwo.fibSeq(1,2)
+    println(functionalProblemTwo.fibSeq(1,2))
+  }
+}
