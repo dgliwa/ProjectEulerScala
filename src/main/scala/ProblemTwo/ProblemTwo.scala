da6eb07bcd4bea18765fef9fa96c908b7ba39174
Tyler Bobella <tjbobella@gmail.com>
2014-12-15 13:14:23 -0700

    Update ProblemTwo.scala
    spacing

--- src/main/scala/ProblemTwo/ProblemTwo.scala
+++ src/main/scala/ProblemTwo/ProblemTwo.scala
@@ -7,17 +7,7 @@ class ProblemTwo {
 
   def fibSeq(first: Int, second: Int): Int = first match{
     case 0 => first
-
     case _ =>
-      (if (second % 2 == 0 ){
-         second
-       } else {
-         0
-       }) + (if (first+second > 4000000){
-        0
-      } else {
-        fibSeq(second, first + second)
-      })
+      (if (second % 2 == 0 ) { second } else { 0 }) + (if (first+second > 4000000){ 0 } else { fibSeq(second, first + second) })
   }
-
 }
